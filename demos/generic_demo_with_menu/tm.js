26ec86f6338fa3c4a358b1d48752c888f3329559
Richard Flynn <richard.flynn@boxever.com>
2021-11-08 14:06:01 +0700

    generic demo w menu - hooking up CDP methods

--- demos/generic_demo_with_menu/tm.js
+++ demos/generic_demo_with_menu/tm.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         offside js
+// @name         generic demo with menu
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
@@ -16,6 +16,79 @@
 
 (function () {
     'use strict';
+    //Boxever settings
+    const BOXEVER_CLIENT_KEY = "pqsSIOPAxhMC9zJLJSZNFURPNqALIFwd";
+    const BOXEVER_POINT_OF_SALE = "spinshop.com";
+    const BOXEVER_API_TARGET = `https://api.boxever.com/v1.2`;
+    const BOXEVER_WEB_FLOW_TARGET = "https://d35vb5cccm4xzp.cloudfront.net";
+    const BOXEVER_JS_LIB_SRC = 'https://d1mj578wat5n4o.cloudfront.net/boxever-1.4.1.min.js';
+
+
+    unsafeWindow._boxever_settings = {
+        client_key: BOXEVER_CLIENT_KEY,
+        target: BOXEVER_API_TARGET,
+        pointOfSale: BOXEVER_POINT_OF_SALE,
+        cookie_domain: '',
+        web_flow_target: BOXEVER_WEB_FLOW_TARGET,
+    };
+
+
+    loadBxLib();
+    delayUntilBrowserIdIsAvailable(sendViewEvent);
+
+
+    function loadBxLib(callback) {
+        console.log('Boxever Tampermonkey script - loadBxLib');
+        var scriptElement = document.createElement('script');
+        scriptElement.type = 'text/javascript';
+        scriptElement.src = BOXEVER_JS_LIB_SRC;
+        scriptElement.async = false;
+        document.head.appendChild(scriptElement);
+    }
+
+    function sendViewEvent() {
+        console.log('Boxever Tampermonkey script - sendViewEvent');
+        var viewEvent = {
+            "browser_id": Boxever.getID(),
+            "channel": "WEB",
+            "type": "VIEW",
+            "language": "EN",
+            "currency": "EUR",
+            "page": unsafeWindow.location.pathname + unsafeWindow.location.search,
+            "pos": BOXEVER_POINT_OF_SALE,
+            "session_data": { "uri": unsafeWindow.location.pathname }
+        };
+        Boxever.eventCreate(viewEvent, function (data) { }, 'json');
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: 'success',
+            title: 'Sent View Event to Sitecore CDP '
+        })
+    }
+
+    function delayUntilBrowserIdIsAvailable(functionToDelay) {
+        if (unsafeWindow.Boxever == null || unsafeWindow.Boxever == undefined || unsafeWindow.Boxever === "undefined" || unsafeWindow.Boxever.getID() === "anonymous") {
+            const timeToWaitInMilliseconds = 300;
+            console.log(`Boxever browserId is not yet available. Waiting ${timeToWaitInMilliseconds}ms before retrying.`);
+            unsafeWindow.setTimeout(delayUntilBrowserIdIsAvailable, timeToWaitInMilliseconds, functionToDelay);
+        } else {
+            functionToDelay();
+        }
+    }
+
+
+
 
     GM_addStyle("/* Offside instances */.offside {    padding: 20px 0;    width: 200px;    background-color: #FFE200;}/* Lists */ul {    clear: both;    margin: 0;    padding: 0 20px;}li {    list-style: none;    margin-left: 1em;}li > a{    text-decoration: none;    color: #000;}/* Offside buttons */.offside-button {    width: 100%;    max-width: 350px;    padding: 10px;    margin: 0 auto 10px auto;    background: #FFE200;    color: #000;    text-align: center;    cursor: pointer;}.offside-button:hover {    background: #FFD900;}.site-overlay {    z-index: 1;    position: absolute;    top: 0;    right: 0;    bottom: 0;    left: 0;    background-color: #FFF;    visibility: hidden;    opacity: 0;    cursor: pointer;    -webkit-transition: visibility 0s ease, opacity .2s ease;    -moz-transition: visibility 0s ease, opacity .2s ease;    -o-transition: visibility 0s ease, opacity .2s ease;    transition: visibility 0s ease, opacity .2s ease;}/* Abstract icon class*/.icon {    display: block;    position: relative;    padding: 10px 0;    background-color: transparent;    background-image: none;    border: 1px solid transparent;}.icon .icon-bar {    display: block;    width: 22px;    height: 2px;    border-radius: 1px;    background-color: #313131;}.icon:hover .icon-bar {    background-color: #989898;}.icon:focus {    outline: 0;}/* Hamburger button */.icon--hamburger .icon-bar + .icon-bar {    margin-top: 4px;}/* Close button */.icon--cross {    padding: 20px;}.icon--cross > .icon-bar + .icon-bar {    -webkit-transform: rotate(45deg);    -ms-transform: rotate(45deg);    -o-transform: rotate(45deg);    transform: rotate(45deg);}.icon--cross > .icon-bar{    -webkit-transform: rotate(-45deg);    -ms-transform: rotate(-45deg);    -o-transform: rotate(-45deg);    transform: rotate(-45deg);    margin-top: -2px;}");
     function injectStylesheet(url) {
@@ -25,7 +98,6 @@
     var htmlStr = "  <button style=\"display:none\" type=\"button\" id=\"my-button\">Offside toggle<\/button>  <nav id=\"my-menu\">  <ul> <li id=\"browsingAsBtn\"><a href=\"#\">Currently browsing as<\/a><\/li>    <li id=\"startAsAnonBtn\"><a href=\"#\">Start as Anon<\/a><\/li>   <li id=\"closeSessionBtn\"><a href=\"#\">Close Session<\/a><\/li>    <li id=\"identifyBtn\" ><a href=\"#\">Identify<\/a><\/li> <li id=\"sendEventBtn\" ><a href=\"#\">Send Event<\/a><\/li> <li id=\"addDataExtBtn\" ><a href=\"#\">Add Data Extension<\/a><\/li> <\/ul>  <\/nav>  <!-- Your Content -->  <div id=\"my-content-container\">    <\/div>";
     var element = document.getElementsByTagName("body")[0];
     element.insertAdjacentHTML('afterbegin', htmlStr);
-
 
     var myOffside = offside('#my-menu', {
 
@@ -45,17 +117,18 @@
 
     });
 
-    function KeyPress(e) {
-        var evtobj = window.event ? event : e
 
+
+    // CTRL+D = toggle menu
+    function KeyPress(e) {
+        var evtobj = unsafeWindow.event ? event : e
         if (evtobj.keyCode == 68 && evtobj.ctrlKey) {
-            // if (confirm('toggle?')) {
             myOffside.toggle();
-            // }
         }
     }
     document.onkeydown = KeyPress;
 
+    // START menu button listners *********************************************
 
     $("#identifyBtn").click(async function () {
 
@@ -115,17 +188,48 @@
             focusConfirm: false,
             showCancelButton: true,
             preConfirm: () => {
-                const login = Swal.getPopup().querySelector('#login').value
-                const password = Swal.getPopup().querySelector('#password').value
-                if (!login || !password) {
-                    Swal.showValidationMessage(`Please enter login and password`)
+                const eventType = Swal.getPopup().querySelector('#eventType').value
+                const kv1 = Swal.getPopup().querySelector('#kv1').value
+                if (!eventType ) {
+                    Swal.showValidationMessage(`Please enter an event type`)
                 }
-                return { login: login, password: password }
+                return { eventType: eventType, kv1: kv1 }
             }
         }).then((result) => {
+            const kv1 = Swal.getPopup().querySelector('#kv1').value;
+            const kv1_values = kv1.split(",");
+
+            const kv2 = Swal.getPopup().querySelector('#kv2').value;
+            const kv2_values = kv2.split(",");
+
+            const kv3 = Swal.getPopup().querySelector('#kv3').value;
+            const kv3_values = kv3.split(",");
+
+            var bxEvent = {
+                "browser_id": Boxever.getID(),
+                "channel": "WEB",
+                "type": result.value.eventType,
+                "language": "EN",
+                "currency": "EUR",
+                "page": unsafeWindow.location.pathname + unsafeWindow.location.search,
+                "pos": BOXEVER_POINT_OF_SALE,
+                "session_data": { "uri": unsafeWindow.location.pathname }
+                
+            };
+            if(kv1 != null && kv1 != undefined && kv1 != "undefeined"){
+                bxEvent[kv1_values[0]] = kv1_values[1];
+            }
+            if(kv2 != null && kv2 != undefined && kv2 != "undefeined"){
+                bxEvent[kv2_values[0]] = kv2_values[1];
+            }
+            if(kv3 != null && kv3 != undefined && kv3 != "undefeined"){
+                bxEvent[kv3_values[0]] = kv3_values[1];
+            }
+           
+            Boxever.eventCreate(bxEvent, function (data) { }, 'json');
             Swal.fire(`
-              Login: ${result.value.login}
-              Password: ${result.value.password}
+              Event Type: ${result.value.eventType}
+              KV1: ${result.value.kv1}
             `.trim())
         })
 
@@ -152,8 +256,8 @@
             cancelButtonAriaLabel: 'Thumbs down'
         })
         function cdpProfileRedirect() {
-            var cdpProfileLink = 'https://app.boxever.com/#/guests/list?filter=customer&q=bid:bidhere'; //Boxever.getID();
-            window.open(cdpProfileLink, '_blank').focus();
+            var cdpProfileLink = 'https://app.boxever.com/#/guests/list?filter=customer&q=bid:' + unsafeWindow.Boxever.getID();
+            unsafeWindow.open(cdpProfileLink, '_blank').focus();
         }
         document.getElementById("cdpProfileRerirectBtn").addEventListener("click", cdpProfileRedirect, false);
 
@@ -171,6 +275,19 @@
             confirmButtonText: 'Yes, close the session'
         }).then((result) => {
             if (result.isConfirmed) {
+                //_boxeverq.push(function () {
+                var closeSessionEvent = {
+                    browser_id: Boxever.getID(),
+                    channel: "WEB",
+                    language: "EN",
+                    currency: "EUR",
+                    pos: BOXEVER_POINT_OF_SALE,
+                    type: "FORCE_CLOSE",
+                    _bx_extended_message: "1"
+                };
+
+                Boxever.eventCreate(closeSessionEvent, function (data) { }, 'json');
+                // });
                 Swal.fire(
                     'Session Closed!',
                     'Your session has been closed.',
@@ -193,16 +310,27 @@
             confirmButtonText: 'Yes, start a new anonymous session'
         }).then((result) => {
             if (result.isConfirmed) {
-                Swal.fire(
-                    'Deleted!',
-                    'Your file has been deleted.',
-                    'success'
-                )
+                //_boxeverq.push(function () {
+                Boxever.reset();
+                //});
+
+                Swal.fire({
+                    title: 'Reload the page to browse as the new anon user',
+                    text: "You won't be able to revert this!",
+                    //icon: 'warning',
+                    showCancelButton: false,
+                    confirmButtonColor: '#3085d6',
+                    confirmButtonText: 'Reload page'
+                }).then((result) => {
+                    location.reload();
+                })
+
+
             }
         })
 
     });
 
-
+    // END menu button listners *********************************************
 
 })();
